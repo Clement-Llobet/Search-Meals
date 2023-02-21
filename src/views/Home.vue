cff219826ff8d0675e06c13af85e55c20a04c416
Clément Llobet <clement.llobet@tutanota.com>
2023-02-21 20:50:24 +0100

    create Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-grey-200 w-full"
+      placeholder="Cherchez votre repas !"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,4 +23,5 @@ import { computed } from "vue";
 import store from "../store";
 
 const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
---
+++ src/views/MealList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Meal List</div>
+</template>
+
+<script setup></script>
