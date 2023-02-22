0ebee35284f27c0743d56e80927568771caeb507
Clément Llobet <clement.llobet@tutanota.com>
2023-02-22 17:35:57 +0100

    design MealsByName vue

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-8 pb-3">
+  <div class="p-8 pb-0">
     <input
       type="text"
       v-model="keyword"
@@ -9,20 +9,52 @@
     />
   </div>
 
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <figure
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <RouterLink to="/">
+        <img
+          :src="meal.strMealThumb"
+          :alt="meal.strMeal"
+          class="rounded-t-xl w-full h-48 object-cover"
+        />
+      </RouterLink>
+      <figcaption class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <div class="flex item-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+            >Youtube</a
+          >
+        </div>
+      </figcaption>
+    </figure>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
-
+import { ref, computed, onMounted } from "vue";
+import { useRoute } from "vue-router";
 import store from "../store";
 
+const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 const searchMeals = () => {
   store.dispatch("searchMeals", keyword.value);
 };
+
+onMounted(() => {
+  keyword.value = route.params.name;
+  keyword.value && searchMeals();
+});
 </script>
