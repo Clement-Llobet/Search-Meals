eae78b57b329363cd6a32c69840cdc8182083720
Clément Llobet <clement.llobet@tutanota.com>
2023-02-22 11:15:37 +0100

    delete useless comments

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-// const axiosService = axios.create({
-//     baseUrl: 'https://www.themealdb.com/api/json/v1/1/',
-// })
-
-// export default axiosService;
 
 export class ApiService {
     constructor(urlBase, instance) {
         this.instance = instance ?? axios.create();
-        this.urlBase = urlBase ?? process.env.SERVICE_BASE_URL;
+        this.urlBase = urlBase;
     }
 
     getAllMeals = async () => {
