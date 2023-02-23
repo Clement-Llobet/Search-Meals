cf0ba2c60bc040d198f5d7fec741f198f0b96a82
Clément Llobet <clement.llobet@tutanota.com>
2023-02-23 11:22:30 +0100

    implement first MealsByLetter view elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="flex flex-col p-8">
-    <div class="flex justify-center gap-2 mt-2">
-      <router-link
-        :to="{ name: 'byLetter', params: { letter } }"
-        v-for="letter of letters"
-        :key="letter"
-      >
-        {{ letter }}
-      </router-link>
-    </div>
-
-    <pre>{{ ingredients }}</pre>
-  </div>
+  <div class="flex flex-col p-8"></div>
 </template>
 
 <script setup>
@@ -21,7 +9,6 @@ import { ApiService } from "../service/apiService";
 
 const api = new ApiService("https://www.themealdb.com/api/json/v1/1/");
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const ingredients = ref([]);
 
 onMounted(async () => {
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>By Letter</div>
+  <div class="flex justify-center gap-2 mt-2">
+    <router-link
+      :to="{ name: 'byLetter', params: { letter } }"
+      v-for="letter of letters"
+      :key="letter"
+    >
+      {{ letter }}
+    </router-link>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
